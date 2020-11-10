9cf600ef23f5b49a626abedd6a4b8b120cccd040
Mike Delano <mdelano@MacBook-Pro.local>
2020-11-10 16:55:15 -0500

    feature: Add support for getting watermarked stream

--- src/api.ts
+++ src/api.ts
@@ -13,27 +13,37 @@ const API_PROTOCOL = "https";
 const API_DOMAIN = "s2-api.safestream.com";
 const API_VERSION = "2.0"
 const API_VIDEOS_PATH = "/videos"
+const API_TEMPLATES_PATH = "/templates"
+const API_WATERMARK_PATH = "/watermark"
 
 export const SafeStream = (config: SafeStreamSDKConfig) => {
+    const AUTH_HEADERS = {
+        'x-api-key': config.auth.apiKey,
+        'x-api-secret': config.auth.apiSecret
+    }
     return {
         getVideos: async () => {
-            try {
-                const response = await axios.get(
-                    `${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_VIDEOS_PATH}`, 
-                    { 
-                        headers: {
-                            'x-api-key': config.auth.apiKey,
-                            'x-api-secret': config.auth.apiSecret
-                        }
-                    }
-                )
-
-                return response.data
-            } catch(e) {
-                console.log(e);
-            } finally {
-                console.log("DONE")
+            const response = await axios.get(`${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_VIDEOS_PATH}`, { headers: AUTH_HEADERS })
+            return response.data
+        },
+        getVideo: async (id: string) => {
+            const response = await axios.get(`${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_VIDEOS_PATH}/${id}`, { headers: AUTH_HEADERS })
+            return response.data
+        },
+        getTemplates: async () => {
+            const response = await axios.get(`${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_TEMPLATES_PATH}/`, { headers: AUTH_HEADERS })
+            return response.data
+        },
+        getStream: async (videoId: string, templateId: string, mappings: object) => {
+            const payload = {
+                videoId: videoId,
+                settingsTemplateMapping: {
+                    id: templateId,
+                    mappings: mappings
+                }
             }
+            const response = await axios.post(`${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_WATERMARK_PATH}/`, payload, { headers: AUTH_HEADERS })
+            return response.data
         }
     }
 }
