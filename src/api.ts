67a38caa59733a3943d38da6bcf5e6dc146c6b2b
Mike Delano <mdelano@MacBook-Pro.local>
2020-11-10 16:22:19 -0500

    Initial commit

---
+++ src/api.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+type SafeStreamSDKAuthConfig = {
+    apiKey: string,
+    apiSecret: string
+}
+
+type SafeStreamSDKConfig = {
+    auth: SafeStreamSDKAuthConfig
+}
+
+const API_PROTOCOL = "https";
+const API_DOMAIN = "s2-api.safestream.com";
+const API_VERSION = "2.0"
+const API_VIDEOS_PATH = "/videos"
+
+export const SafeStream = (config: SafeStreamSDKConfig) => {
+    return {
+        getVideos: async () => {
+            const videos = await axios.get(
+                `${API_PROTOCOL}://${API_DOMAIN}/${API_VERSION}${API_VIDEOS_PATH}`, 
+                { 
+                    headers: {
+                        'x-api-key': config.auth.apiKey,
+                        'x-api-secret': config.auth.apiKey
+                    }
+                }
+            )
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                console.log("DONE")
+            });
+             
+            return videos;
+        }
+    }
+}
